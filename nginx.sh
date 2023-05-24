3c1fe6b164c3df1d34c1af2c76fbafe94a5d9263
engrsketch <dalusianyi@gmail.com>
2023-05-24 14:58:33 +0100

    clear repo

--- nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600" -H "X-aws-ec2-metadata-token: true"`
-instance_id=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
-echo "Hello from ${HOSTNAME}. This instnace ID is ${instance_id}" >> /usr/share/nginx/html/index.html
-#echo "Hello from ${instance_id}" >> /usr/share/nginx/html/index.html
--- roles.tf
+++
@@ -1,5 +0,0 @@
-# Auto scaling service link role
-resource "aws_iam_service_linked_role" "autoscaling" {
-  custom_suffix    = "${var.env}-engrsketch"
-  aws_service_name = "autoscaling.amazonaws.com"
-}
